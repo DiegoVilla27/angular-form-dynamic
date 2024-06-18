c2df82cca6ae08f3a4eca0e65b16caafb114d452
Diego Villa <dvillanueva@sdi.es>
2024-06-18 18:54:51 +0200

    feat: Delete a pipe unnecesary

--- src/app/pipes/validate-input.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ValidateInputPipe } from "./validate-input.pipe";
-
-describe("ValidateInputPipe", () => {
-  it("create an instance", () => {
-    const pipe = new ValidateInputPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
